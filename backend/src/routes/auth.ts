081f712f1eb7230356fce5b11af7317ca7e23f02
avi648elastic-dot <avi648elastic@gmail.com>
2025-10-04 21:59:05 +0300

    Update auth.ts

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -27,6 +27,7 @@ router.post('/signup', async (req: Request, res: Response) => {
       return res.status(400).json({ message: 'User already exists' });
     }
 
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = new User({
