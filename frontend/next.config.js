db172b245cbd7c849ecdfde5a54050554825b1be
avi648elastic-dot <avi648elastic@gmail.com>
2025-10-15 14:43:55 +0300

    Fix X-Frame-Options to allow SAMEORIGIN instead of DENY

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -108,7 +108,7 @@ const nextConfig = {
           },
           {
             key: 'X-Frame-Options',
-            value: 'DENY',
+            value: 'SAMEORIGIN',
           },
           {
             key: 'X-XSS-Protection',
